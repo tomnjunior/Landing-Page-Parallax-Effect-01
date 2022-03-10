a49fc3a34106eb7b7ed56201567fe33d9824dd91
Tom Nonato Junior <tomnjunior@gmail.com>
2022-03-10 01:06:43 -0300

    Landing-Page-Parallax-Effect-01

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+//------Parallax Effect------//
+
+gsap.to("#bg", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    scale : 1.5
+});
+
+gsap.to("#mineirao", {
+    scrollTrigger : {
+        scrub : 1
+    },
+   y : 200,
+});
+
+gsap.to("#montanha", {
+    scrollTrigger : {
+        scrub : 1
+    },
+   y : 500,
+});
+
+gsap.to("#igreja", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : -1500,
+});
+
+gsap.to("#praca", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 1500,
+});
+
+gsap.to("#nuvem21", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 200,
+});
+
+gsap.to("#nuvem22", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 200,
+});
+
+gsap.to("#nuvem11", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : -200,
+});
+
+gsap.to("#nuvem12", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : -200,
+});
+
+gsap.to("#estatuas", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 1500,
+});
+
+gsap.to("#text", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    scale : 0.5,
+});
